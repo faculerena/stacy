54f42c1d412d0f561b6aff431d1bdd0b6f83e2e3
Facundo Lerena <faculerena@gmail.com>
2024-05-31 17:29:08 -0300

    typo

--- stacks_analyzer/stacks_analyzer.py
+++ stacks_analyzer/stacks_analyzer.py
@@ -39,7 +39,7 @@ def main():
             "AssertBlockHeight",
             "CallInsideAsContract",
             "DivideBeforeMultiply",
-            "PrivateFunctionOnlyNotUsed",
+            "PrivateFunctionNotUsed",
             "TxSenderInAssert",
             "UnwrapPanicUsage",
             "VarCouldBeConstant"
